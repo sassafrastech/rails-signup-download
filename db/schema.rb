5bbbb0665ab6d078d4c26fe64e6ccdbb2db4fd6e
Jill Dimond <jill@sassafrastech.com>
2017-06-07 13:29:57 -0400

    updatded readme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102122811) do
+ActiveRecord::Schema.define(version: 20140708220535) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,9 +27,8 @@ ActiveRecord::Schema.define(version: 20150102122811) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
