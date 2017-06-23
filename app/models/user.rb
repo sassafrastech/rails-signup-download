613f1af52e9347fccde0295405ebb9e7721e23a1
Kyla Massey <sir.massey@gmail.com>
2017-06-22 21:26:23 -0400

    added avatar validation and uploader in class User, models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
   after_create :sign_up_for_mailing_list
+  mount_uploader :avatar, AvatarUploader
 
   def set_default_role
     self.role ||= :user
@@ -11,6 +12,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # User Avatar Validation - to check integrity and processing of avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
 
   def sign_up_for_mailing_list
     MailingListSignupJob.perform_later(self)
@@ -26,5 +31,10 @@ class User < ApplicationRecord
     })
     Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
   end
+
+  private
+    def avatar_size_validation
+      errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+    end
 
 end
