2884cbe0d9fdb80058dcd5ec89a4dbd9a2ae7fd1
Kyla Massey <sir.massey@gmail.com>
2017-06-22 23:08:02 -0400

    git commit -m added remove_avatar to list of accessible attributes in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:email, :password, :password_confirmation, :remember_me, :avatar, :avatar, :avatar_cache]
+    added_attrs = [:email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
