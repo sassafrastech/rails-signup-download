0e9a55e4bfaa2aacca7e3bbc73a2e6c038b1ff36
Kyla Massey <sir.massey@gmail.com>
2017-06-23 19:43:15 -0400

    added default image to assets and avatar_uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -22,6 +22,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default_image.png"].compact.join('_'))
+  end
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
